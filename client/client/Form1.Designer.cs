4119cf96e426a8a66e671afd36b20c5cf37d3dcd
ecekarasu <ecekarasu@sabanciuniv.edu>
2022-11-17 20:54:15 +0300

    initial commit

---
+++ client/client/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_ip = new System.Windows.Forms.TextBox();
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.textBox_email = new System.Windows.Forms.TextBox();
+            this.textBox_name = new System.Windows.Forms.TextBox();
+            this.logs = new System.Windows.Forms.RichTextBox();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_ip
+            // 
+            this.textBox_ip.Location = new System.Drawing.Point(12, 37);
+            this.textBox_ip.Name = "textBox_ip";
+            this.textBox_ip.Size = new System.Drawing.Size(100, 26);
+            this.textBox_ip.TabIndex = 0;
+            this.textBox_ip.Tag = "";
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(12, 69);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(100, 26);
+            this.textBox_port.TabIndex = 1;
+            // 
+            // textBox_email
+            // 
+            this.textBox_email.Location = new System.Drawing.Point(12, 101);
+            this.textBox_email.Name = "textBox_email";
+            this.textBox_email.Size = new System.Drawing.Size(100, 26);
+            this.textBox_email.TabIndex = 2;
+            // 
+            // textBox_name
+            // 
+            this.textBox_name.Location = new System.Drawing.Point(12, 133);
+            this.textBox_name.Name = "textBox_name";
+            this.textBox_name.Size = new System.Drawing.Size(100, 26);
+            this.textBox_name.TabIndex = 3;
+            // 
+            // logs
+            // 
+            this.logs.Location = new System.Drawing.Point(118, 69);
+            this.logs.Name = "logs";
+            this.logs.Size = new System.Drawing.Size(148, 163);
+            this.logs.TabIndex = 4;
+            this.logs.Text = "";
+            // 
+            // button_connect
+            // 
+            this.button_connect.AccessibleName = "";
+            this.button_connect.Location = new System.Drawing.Point(142, 32);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(100, 37);
+            this.button_connect.TabIndex = 5;
+            this.button_connect.Text = "connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.logs);
+            this.Controls.Add(this.textBox_name);
+            this.Controls.Add(this.textBox_email);
+            this.Controls.Add(this.textBox_port);
+            this.Controls.Add(this.textBox_ip);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_ip;
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.TextBox textBox_email;
+        private System.Windows.Forms.TextBox textBox_name;
+        private System.Windows.Forms.RichTextBox logs;
+        private System.Windows.Forms.Button button_connect;
+    }
+}
+
---
+++ server/server/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace server
+{
+    public partial class Form1 : Form
+    {
+
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        List<string> connectedUsers = new List<string>(); // keeps the names of connected users
+        Dictionary<string, Socket> clientSocketDict = new Dictionary<string, Socket>(); // keeps name -> client tuples
+
+        bool terminating = false;
+        bool listening = false;
+
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+            InitializeComponent();
+        }
+
+        private void button_listen_Click(object sender, EventArgs e)
+        {
+            int serverPort;
+
+            if (Int32.TryParse(textBox_port.Text, out serverPort))
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, serverPort);
+                serverSocket.Bind(endPoint);
+                serverSocket.Listen(3);
+
+                listening = true;
+                button_listen.Enabled = false;
+                //textBox_message.Enabled = true;
+                //button_send.Enabled = true;
+
+                Thread acceptThread = new Thread(Accept);
+                acceptThread.Start();
+
+                richTextBox_info.AppendText("Started listening on port: " + serverPort + "\n");
+            }
+            else
+            {
+                richTextBox_info.AppendText("Please check port number \n");
+            }
+        }
+        private void Accept()
+        {
+            while (listening)
+            {
+                try
+                {
+                    string name = ""; // we initialize name to empty string
+                    Socket newClient = serverSocket.Accept(); // first we accept the new connection request
+                    if (checkClient(newClient, ref name))
+                    { // gets the name and check if name is registered
+                        if (!clientSocketDict.ContainsKey(name)) // checks if the user already connected
+                        {
+                            send_message(newClient, "authorized\n");
+                            clientSocketDict.Add(name, newClient);
+                            connectedUsers.Add(name);
+                            richTextBox_info.AppendText(name + " is connected.\n");
+                            richTextBox_info.ScrollToCaret();
+                            foreach (string clientName in connectedUsers)
+                            {
+                                if (clientName != name) // do not send it to sender client
+                                {
+                                    Socket tempSocket = clientSocketDict[clientName]; // we got the socket
+                                    send_message(tempSocket, (name + " is connected\n"));
+                                }
+                            }
+                            Thread receiveThread = new Thread(Receive);
+                            receiveThread.Start();
+                        }
+                        else
+                        {
+                            richTextBox_info.AppendText(name + " is trying to connect again\n");
+                            richTextBox_info.ScrollToCaret();
+                            send_message(newClient, "already connected");
+                            newClient.Close();
+                        }
+                    }
+                    else
+                    {
+                        richTextBox_info.AppendText(name + " is trying to connect but not registered\n");
+                        richTextBox_info.ScrollToCaret();
+                        send_message(newClient, "not authorized");
+                        newClient.Close();
+                    }
+                }
+                catch
+                {
+                    if (terminating)
+                    {
+                        listening = false;
+                    }
+                    else
+                    {
+                        richTextBox_info.AppendText("The socket stopped working.\n");
+                        richTextBox_info.ScrollToCaret();
+                    }
+
+                }
+            }
+        }
+        private bool checkClient(Socket thisClient, ref string name) // gets the name of user and returns that users registiration status
+        {
+            try
+            {
+                string incomingName = receiveOneMessage(thisClient); // get the name
+                if (connectedUsers.Contains(incomingName)) // check if name is registered
+                {
+                    name = incomingName;
+                    return true;
+                }
+                else
+                {
+                    name = incomingName;
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                richTextBox_info.AppendText("Fail: " + ex.ToString() + "\n");
+                richTextBox_info.ScrollToCaret();
+                throw;
+            }
+        }
+        private string receiveOneMessage(Socket clientSocket) // this function receives only one message and returns it
+        {
+            Byte[] buffer = new Byte[10000000];
+            clientSocket.Receive(buffer);
+            string incomingName = Encoding.Default.GetString(buffer);
+            incomingName = incomingName.Substring(0, incomingName.IndexOf("\0"));
+            return incomingName;
+        }
+        private void send_message(Socket clientSocket, string message) // takes socket and message then sends the message to that socket
+        {
+            Byte[] buffer = new Byte[10000000];
+            buffer = Encoding.Default.GetBytes(message);
+            clientSocket.Send(buffer);
+        }
+        private void Receive() // updated
+        {
+            bool connected = true;
+            string name = connectedUsers[connectedUsers.Count() - 1]; // we got the username
+            Socket thisClient = clientSocketDict[name]; // we got the socket that related to the username
+
+            while (connected && !terminating)
+            {
+                try
+                {
+                    Byte[] buffer = new Byte[64];
+                    thisClient.Receive(buffer);
+
+                    string incomingName = Encoding.Default.GetString(buffer);
+                    incomingName = incomingName.Substring(0, incomingName.IndexOf("\0"));
+                    richTextBox_info.AppendText("Client: " + incomingName + "\n");
+                }
+                catch
+                {
+                    if (!terminating)
+                    {
+                        richTextBox_info.AppendText("A client has disconnected\n");
+                    }
+                    thisClient.Close();
+                    clientSocketDict.Remove(name);
+                    connected = false;
+                }
+            }
+        }
+        private void Form1_FormClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            listening = false;
+            terminating = true;
+            Environment.Exit(0);
+        }
+    }
+}
